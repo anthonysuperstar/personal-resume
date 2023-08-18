bc95157a85e750349357dccd61027d310e697a09
antho <anton.mula01@gmail.com>
2023-08-18 18:37:43 +0200

    nav-mobile

--- src/components/NavbarMobile/NavbarMobile.tsx
+++ src/components/NavbarMobile/NavbarMobile.tsx
@@ -28,11 +28,86 @@ export default function NavbarMobile(){
         !visible? setVisible(true) : setVisible(false)
     }
 
+    const content = {
+        en : [
+            {
+                link: "welcome",
+                name: "Home"
+            },
+            {
+                link: "intro",
+                name: "About Me"
+            },
+            {
+                link: "sk",
+                name: "Skills"
+            },
+            {
+                link: "background",
+                name: "Background"
+            },
+            {
+                link: "portfolio",
+                name: "Portfolio"
+            },
+            {
+                link: "resume",
+                name: "Resume"
+            }
+        ],
+        fr: [
+            {
+                link: "welcome",
+                name: "Accueil"
+            },
+            {
+                link: "intro",
+                name: "A propos de moi"
+            },
+            {
+                link: "sk",
+                name: "Compétences"
+            },
+            {
+                link: "background",
+                name: "Parcours"
+            },
+            {
+                link: "portfolio",
+                name: "Portfolio"
+            },
+            {
+                link: "resume",
+                name: "CV"
+            }
+        ]
+    }
+    
+    const en_mapped = content.en.map((link) => {
+        return (
+            <tr>
+                <Link className="na-li-mobile" to={link.link} key={link.name} spy={true} smooth={true} onClick={()=>{setVisible(false)}} offset={0} duration={1000}>{link.name}</Link>
+            </tr>
+        ) 
+    })
+    const fr_mapped = content.fr.map((link) => {
+        return (
+            <tr>
+                <Link className="na-li-mobile" to={link.link} key={link.name} spy={true} smooth={true} onClick={()=>{setVisible(false)}} offset={0} duration={1000}>{link.name}</Link>
+            </tr>
+        )
+    })
+
+
     return(
-        <div className="nav-mobile">
+        <div className={!visible? "nav-mobile closed" : "nav-mobile"}>
             <div className="burger-icon">
                 <img src={!visible? burger : cross} onClick={handleClickMenu} alt="" />
             </div>
+            <table className={!visible? "menu none" : "menu"}>
+                {language === "en"? en_mapped : fr_mapped}
+            </table>
+            
         </div>
     )
 }
